8302d40c857e80c3e116132f94f6c0a16461c9fd
Drew Parker <drewisdad@gmail.com>
2012-02-15 22:04:29 +1100

    Added Stylesheets and Testing criteria

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Quick-Score.com"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} @ #{base_title}"
+    end
+  end
+
 end
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,28 +1,58 @@
 require 'spec_helper'
 
 describe "Pages" do
-  describe "Home Page" do
-    it "should have the content 'Home'" do
+
+  let(:base_title) { "Quick-Score.com" }
+
+  describe "Home page" do
+
+    it "should have the h1 'Home'" do
       visit '/pages/home'
-      page.should have_content('Home')
+      page.should have_selector('h1', :text => 'Home')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/pages/home'
+      page.should have_selector('title', :text => "Home @ #{base_title}")
     end
   end
-  describe "About Page" do
-    it "should have the content 'About Us'" do
-      visit '/pages/about'
-      page.should have_content('About Us')
-    end
-  end
-  describe "Contact Page" do
-    it "should have the content 'Contact Us'" do
-      visit '/pages/contact'
-      page.should have_content('Contact Us')
-    end
-  end
-  describe "Privacy Page" do
-    it "should have the content 'Privacy'" do
+
+  describe "Privacy page" do
+
+    it "should have the h1 'Privacy'" do
       visit '/pages/privacy'
-      page.should have_content('Privacy')
+      page.should have_selector('h1', :text => 'Privacy')
+    end
+
+    it "should have the title 'Privacy'" do
+      visit '/pages/privacy'
+      page.should have_selector('title', :text => "Privacy @ #{base_title}")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/pages/about'
+      page.should have_selector('title', :text => "About Us @ #{base_title}")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact Us'" do
+      visit '/pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact Us'" do
+      visit '/pages/contact'
+      page.should have_selector('title', :text => "Contact Us @ #{base_title}")
     end
   end
 end
